fe65bf9b7f4fc300275650ef544015dcf1f386ff
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-14 20:54:53 -0500

    Seeding, adding posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+
+
 class HomeController < ApplicationController
   before_action :authenticate_user, only: [:dashboard]
 
@@ -10,6 +12,9 @@ class HomeController < ApplicationController
     timeline = ids_of_my_followers << @current_user.id
     @manoises = Manoise.where(user_id: timeline).order("created_at DESC").page params[:page]
     @users = User.all
+
+    puts "---"*200
+    print @users[0].manoises[0]
                 #  .reject {|u| @current_user.following? u}
                 #  .reject {|u| @current_user == u}
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :manoises
   has_secure_password
-  validates :username, :post, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
 
   acts_as_followable
   acts_as_follower
