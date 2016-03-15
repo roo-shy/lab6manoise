3028c09d8ed9b68a5cf5ee07000127d81dc1aae7
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-14 21:16:41 -0500

    Running Sign in and Sign up tests

--- test/features/can_sign_in_test.rb
+++ test/features/can_sign_in_test.rb
@@ -5,10 +5,10 @@ class CanSignInTest < Capybara::Rails::TestCase
     me = User.create! password: "password", username: "user"
 
     visit root_path
-    click_button "Sign In" #, className: :btn
+    click_on "Sign In" #, className: :btn
     fill_in "Username", with: "user"
     fill_in "Password", with: "password"
-    click_button "Sign In"
+    click_on "Sign In"
     assert_content page, "New Posts"
 
   end
--- test/features/can_sign_up_test.rb
+++ test/features/can_sign_up_test.rb
@@ -3,12 +3,12 @@ require "test_helper"
 class CanSignUpTest < Capybara::Rails::TestCase
   test "can sign up" do
     visit root_path
-    click_submit "Sign In"
-    click_button "Sign Up"
+    click_on "Sign In"
+    click_on "Sign Up"
     fill_in "Username", with: "ZACK"
     fill_in "Password", with: "123456789"
     fill_in "Password Confirmation", with: "123456789"
-    click_button "Sign Up"
+    click_on "Sign Up"
 
     assert_content page, "ZACK"
 
